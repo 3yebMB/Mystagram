68f9489ee344eaf2ae494b4c60b27ec31e6e755a
Michael <3yebmb@gmail.com>
2019-10-02 17:26:51 +0500

    add fragments and navigation

--- app/src/main/java/dev/m13d/mystagram/HomeActivity.kt
+++ app/src/main/java/dev/m13d/mystagram/HomeActivity.kt
@@ -3,8 +3,24 @@ package dev.m13d.mystagram
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_home.*
+import androidx.core.app.ComponentActivity
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+
 
 class HomeActivity : AppCompatActivity() {
+
+    val fragment1: Fragment = HomeFragment()
+    val fragment2: Fragment = SearchFragment()
+    val fragment3: Fragment = AddFragment()
+    val fragment4: Fragment = FavFragment()
+    val fragment5: Fragment = AccFragment()
+
+    val fm: FragmentManager = supportFragmentManager
+    var active = fragment1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,5 +33,11 @@ class HomeActivity : AppCompatActivity() {
         for (i in 0 until bottom_navigation_view.menu.size()) {
             bottom_navigation_view.setIconTintList(i, null)
         }
+        fm.beginTransaction().add(R.id.main_container, fragment5, "5").hide(fragment5).commit()
+        fm.beginTransaction().add(R.id.main_container, fragment4, "4").hide(fragment4).commit()
+        fm.beginTransaction().add(R.id.main_container, fragment3, "3").hide(fragment3).commit()
+        fm.beginTransaction().add(R.id.main_container, fragment2, "2").hide(fragment2).commit()
+        fm.beginTransaction().add(R.id.main_container, fragment1, "1").commit()
+
     }
 }
