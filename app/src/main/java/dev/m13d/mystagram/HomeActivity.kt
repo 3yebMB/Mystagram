396711cb6149a743b75a20c3ef5bcdda0ff78ae8
Michael <3yebmb@gmail.com>
2019-10-04 13:29:56 +0500

    add navigation between fragments

--- app/src/main/java/dev/m13d/mystagram/HomeActivity.kt
+++ app/src/main/java/dev/m13d/mystagram/HomeActivity.kt
@@ -1,43 +1,77 @@
 package dev.m13d.mystagram
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_home.*
-import androidx.core.app.ComponentActivity
-import androidx.core.app.ComponentActivity.ExtraData
-import androidx.core.content.ContextCompat.getSystemService
-import android.icu.lang.UCharacter.GraphemeClusterBreak.T
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentTransaction
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeActivity : AppCompatActivity() {
 
-    val fragment1: Fragment = HomeFragment()
-    val fragment2: Fragment = SearchFragment()
-    val fragment3: Fragment = AddFragment()
-    val fragment4: Fragment = FavFragment()
-    val fragment5: Fragment = AccFragment()
+    private val TAG = "HomeActivity"
 
-    val fm: FragmentManager = supportFragmentManager
-    var active = fragment1
+    lateinit var homeFragment: HomeFragment
+    lateinit var searchFragment: SearchFragment
+    lateinit var addFragment: AddFragment
+    lateinit var favFragment: FavFragment
+    lateinit var accFragment: AccFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-        bottom_navigation_view.setTextVisibility(false)
-        bottom_navigation_view.setIconSize(29f, 29f)
-        bottom_navigation_view.enableItemShiftingMode(false)
-        bottom_navigation_view.enableShiftingMode(false)
-        bottom_navigation_view.enableAnimation(false)
-        for (i in 0 until bottom_navigation_view.menu.size()) {
-            bottom_navigation_view.setIconTintList(i, null)
-        }
-        fm.beginTransaction().add(R.id.main_container, fragment5, "5").hide(fragment5).commit()
-        fm.beginTransaction().add(R.id.main_container, fragment4, "4").hide(fragment4).commit()
-        fm.beginTransaction().add(R.id.main_container, fragment3, "3").hide(fragment3).commit()
-        fm.beginTransaction().add(R.id.main_container, fragment2, "2").hide(fragment2).commit()
-        fm.beginTransaction().add(R.id.main_container, fragment1, "1").commit()
 
+        homeFragment = HomeFragment()
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.frame_layout, homeFragment)
+            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+            .commit()
+
+        val bottomNavigation: BottomNavigationView = findViewById(R.id.bottom_navigation_view)
+
+        bottomNavigation.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.item_home -> {
+                    homeFragment = HomeFragment()
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.frame_layout, homeFragment)
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .commit()
+                }
+                R.id.item_search -> {
+                    searchFragment = SearchFragment()
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.frame_layout, searchFragment)
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .commit()
+                }
+                R.id.item_add -> {
+                    addFragment = AddFragment()
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.frame_layout, addFragment)
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .commit()
+                }
+                R.id.item_fav -> {
+                    favFragment = FavFragment()
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.frame_layout, favFragment)
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .commit()
+                }
+                R.id.item_acc -> {
+                    accFragment = AccFragment()
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.frame_layout, accFragment)
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .commit()
+                }
+            }
+            true
+        }
     }
 }
